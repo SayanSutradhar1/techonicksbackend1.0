196a06fccd3f682ba049aef7630028b8019d5d5c
SayanSutradhar1 <sayan321x@gmail.com>
2024-12-02 18:33:06 +0530

    minor

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,3 +1,4 @@
 export function demoController(req,res){
-    res.redirect('https://techonicks.vercel.app/')
+    // res.redirect('https://techonicks.vercel.app/')
+    res.send("You are not allowed to this page")
 }
